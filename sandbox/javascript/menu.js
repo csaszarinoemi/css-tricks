268f6384e3cb55eb5b375c17f55ffe16a5a47e68
Csaszari Noemi <noemi.csaszari@schibsted.com>
2021-06-20 13:41:17 +0200

    Create sliding menu

---
+++ sandbox/javascript/menu.js
@@ -0,0 +1,11 @@
+const toggler = document.querySelector('.menu__toggler');
+const menu    = document.querySelector('.menu');
+
+/*
+ * Toggles on and off the 'active' class on the menu
+ * and the toggler button.
+ */
+toggler.addEventListener('click', () => {
+  toggler.classList.toggle('active');
+  menu.classList.toggle('active');
+})
